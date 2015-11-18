5312f38cb3de28c7f0032f43ce684a2e40d9def8
Samy VACHERESSE <samy@samyv.com>
2015-11-18 11:01:24 +0100

    init

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+  $position = "Fullstack developer";
+  $techno = "Ruby on Rails, ReactJS, Elixir, Golang";
+?>
+
+
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>SAMY VACHERESSE - <?php echo $position ?></title>
+    <meta name="description" content="<?php echo $techno ?> ">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    
+</head>
+<body>
+    <!--[if lt IE 8]>
+        <p class="chromeframe center">Vous utilisez un navigateur internet suranné. Pour votre sécurité et votre confort de navigation <a href="http://browsehappy.com/">mettez à jour votre navigateur aujourd'hui</a>.
+    <![endif]-->
+
+
+
+
+
+<!--Start of Zopim Live Chat Script-->
+<!--
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='//v2.zopim.com/?1pgdSUESao57lKkrZgcUkzJUXWZwaGmY';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+</script>
+-->
+<!--End of Zopim Live Chat Script-->
+
+<!--Start of Google Analitics-->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32389341-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+    
+</body>
+</html>
